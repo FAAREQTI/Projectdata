064b3e6f59003b0bbada3cffc961e43fdbad19de
FAAREQTI <fezzahraareqti@gmail.com>
2023-04-22 22:51:26 +0000

    nv utils

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,27 @@ from typing import List, Tuple
 import argparse
 import os
 from pathlib import Path
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 env_path = Path('.env')
 load_dotenv(env_path)
 
 # function to connect to database
+
+
 def connet_db(database: str) -> Tuple[str, str]:
+    """Connects to a PostgreSQL database with the given database name
+    Args:
+        database (str): the name of the database
+    Returns:
+        Tuple[str, str]: tuple containing the connection and cursor objects.
+    """
     try:
         conn = psycopg2.connect(
             host="localhost",
             database=database,
             user="postgres",
-            password= os.getenv("PASSWORD")
+            password=os.getenv("PASSWORD")
         )
         cur = conn.cursor()
     except Exception as e:
@@ -27,6 +35,13 @@ def connet_db(database: str) -> Tuple[str, str]:
 
 
 def create_table(cur: str, col_type: str, name_of_table: str, conn: str):
+    """Creates a new table in a PostgreSQL database
+    Args:
+        cur (str): cursor object
+        col_type (str): colum corresponding data types 
+        name_of_table (str): name of table to be created
+        conn (str): the connection object
+    """
     try:
         # drop the table if exists
         cur.execute(f"DROP TABLE IF EXISTS {name_of_table}")
@@ -38,6 +53,12 @@ def create_table(cur: str, col_type: str, name_of_table: str, conn: str):
         print("Error:", e)
 
 # load csv
+    """Loads a CSV file as a pandas DataFrame.
+     Args:
+        filename: path to csv file
+    Returns:
+        df: the CSV file as a pandas DataFrame
+    """
 
 
 def load_csv(filename: str) -> pd.DataFrame:
@@ -51,6 +72,12 @@ def load_csv(filename: str) -> pd.DataFrame:
 
 
 def convert_types_to_sql_format(df: pd.DataFrame) -> Tuple[str, str]:
+    """Converts pandas data types to SQL data types.
+    Args:
+        df (pd.DataFrame): Pandas dataframe to be converted
+    Returns:
+        Tuple[str, str]: tuple contaning SQL column names and values
+    """
     try:
         types = []
         for i in df.dtypes:
