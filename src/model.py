f841faac9afb8a672f128a74ed48fe9f95594652
FAAREQTI <fezzahraareqti@gmail.com>
2023-04-28 18:11:03 +0000

    modelpy

--- src/model.py
+++ src/model.py
@@ -1,21 +1,26 @@
 import pandas as pd
 from prophet import Prophet
 
+
 def prophet_forecast(df):
     city_list = df['city'].unique()
     product_list = df['product_line'].unique()
     results = []
-    for city in city_list: 
+    for city in city_list:
         for product_line in product_list:
-            df_filter = df[(df['city'] == city) & (df['product_line'] == product_line)]
-            df_filter = df_filter[['date', 'quantity']].rename(columns={'date': 'ds', 'quantity': 'y'})
+            df_filter = df[(df['city'] == city) & (
+                df['product_line'] == product_line)]
+            df_filter = df_filter[['date', 'quantity']].rename(
+                columns={'date': 'ds', 'quantity': 'y'})
             model = Prophet()
             model.fit(df_filter)
             future_dates = model.make_future_dataframe(periods=60)
             forecast = model.predict(future_dates)
             forecast['city'] = city
             forecast['product_line'] = product_line
-            results.append(forecast[["ds","yhat","city","product_line"]])
-    forecast = pd.concat(results,ignore_index = True)
+            results.append(forecast[["ds", "yhat", "city", "product_line"]])
+    forecast = pd.concat(results, ignore_index=True)
     return forecast
 
+
+prophet_forecast(df_filter)
