f8a062b7fb9e8ae669f3f488c21955c47a966989
FAAREQTI <fezzahraareqti@gmail.com>
2023-04-29 23:29:36 +0000

    changes

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from prophet import Prophet
-from src.utils import read_file_from_s3 
+from neuralprophet import NeuralProphet
+from utils import read_file_from_s3, upload_to_google_sheet 
 
 def prophet_forecast(df):
     city_list = df['city'].unique()
@@ -12,20 +12,25 @@ def prophet_forecast(df):
                 df['product_line'] == product_line)]
             df_filter = df_filter[['date', 'quantity']].rename(
                 columns={'date': 'ds', 'quantity': 'y'})
-            model = Prophet()
-            model.fit(df_filter)
-            future_dates = model.make_future_dataframe(periods=60)
+            model = NeuralProphet()
+            df_filter=df_filter.drop_duplicates(["ds"], keep="first")
+            model.fit(df_filter, freq="D")
+            future_dates = model.make_future_dataframe(df_filter, n_historic_predictions=True, periods=60)
             forecast = model.predict(future_dates)
             forecast['city'] = city
             forecast['product_line'] = product_line
-            results.append(forecast[["ds", "yhat", "city", "product_line"]])
+            results.append(forecast[["ds", "yhat1", "city", "product_line"]])
     forecast = pd.concat(results, ignore_index=True)
     return forecast
 
 
 # add to py script
 df = read_file_from_s3("supermarket.csv")
-df.head()
 
 # execute your function and assign it to a variable as function has return statement
 output = prophet_forecast(df)
+
+spreadsheet_id = "1xjPE5wUM7dd5JuZaZXoEIKMgeOJpNcZ5i_uw7EXwQIk"
+worksheet_name = "supermarket"
+
+upload_to_google_sheet(spreadsheet_id, output, worksheet_name)    
